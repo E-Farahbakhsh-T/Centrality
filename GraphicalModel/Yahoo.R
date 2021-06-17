a3770a9a6878756568851c9e058409b6dea61d95
Elena Farahbakhsh Touli <elena.touli@math.su.se>
2021-06-17 18:29:00 +0200

    Yahoo was updated

--- GraphicalModel/Yahoo.R
+++ GraphicalModel/Yahoo.R
@@ -2,9 +2,10 @@
 # The code has been completed but the only problem is that I assumed every mst has just one center. 
  
 
-
-
-
+mycomination <- function(n,c)
+{
+  factorial(n)/(factorial(c)*factorial(n-c))
+}
 
 SimpleReturnFunction <- function(DataMatrix, dict)
 {
@@ -14,20 +15,11 @@ SimpleReturnFunction <- function(DataMatrix, dict)
   
   for (i in 1:r-1)
   {
-    SimpleReturnDataMatrix[i,] = (DataMatrix[i+1,]-DataMatrix[i,])/DataMatrix[i,]
+    SimpleReturnDataMatrix[i,] = (DataMatrix[i,] - DataMatrix[i+1,])/DataMatrix[i+1,]
   }
   r = dim(SimpleReturnDataMatrix)[1]
   
   #### Now, we have the data as a matrix of numbers
-  LogReturnDataMatrix = matrix(0, nrow=r, ncol=c)
-  for(i in 1:r)
-  {
-    for(j in 1:c)
-    {
-      LogReturnDataMatrix <- log(1+SimpleReturnDataMatrix)
-    }
-    
-  }
   w = 252 # for one year without holidays
   totaltime = r - w + 1
   
@@ -85,25 +77,26 @@ SimpleReturnFunction <- function(DataMatrix, dict)
   #https://sites.google.com/site/daishizuka/toolkits/sna/weighted-edges
   library(igraph)
   install.packages(igraph)
-  
+
 
   
   
   # every time we want to find the center of the mst of a graph
   center = c()
-  
-  
+  diam = c()
+  distancefromstar = c()
   
   for (j in 1:totaltime)
   {
-    net=graph.adjacency(dMatrix[j,,],mode="undirected",weighted=TRUE,diag=FALSE)
-  
+    net = graph.adjacency(dMatrix[j,,],mode="undirected",weighted=TRUE,diag=FALSE)
+    diam  <-  append(diam, diameter(net))
     
     # we can plot here but we behöver inte
     #plot.igraph(net,vertex.label=V(net)$name,layout=layout.fruchterman.reingold, edge.color="black",edge.width=E(net)$weight)
     
     # finding the min spanning tree
     mstnet = minimum.spanning.tree(net)
+    degreevec <- degree(mstnet)
     
     # plot the mst graph
     #plot.igraph(mstnet,vertex.label=V(net)$name,layout=layout.fruchterman.reingold, edge.color="black",edge.width=E(net)$weight)
@@ -123,19 +116,37 @@ SimpleReturnFunction <- function(DataMatrix, dict)
     center <- append(center, dict[[t]])
     #center <- append(center, t)
     #print(which.min(maxdist))
-  }
     
-  return(center)
-  
+    #sumcom <- 0
+    
+    if (degreevec[[t]] == length(dict)-1){
+      distancefromstar <- append(distancefromstar, 1)
+    }else{
+      distancefromstar <- append(distancefromstar, 0)
+    }
+    
+    # for (i in 1:c)
+    # {
+    #   if (degreevec[[i]]>1)
+    #   {
+    #     sumcom <- sumcom + mycomination(degreevec[[i]],2)
+    #     }
+    # }
+    # distancefromstar <- append(distancefromstar, mycomination(degreevec[[t]],2)/sumcom)
+    
+    
+  }
+   
+   
+  #plot (distancefromstar)
   write.csv(center, file = "/Users/asanapple/Desktop/Research/21-05-01/Simplereturncenters.csv")
   
   # The code has been completed but the only problem is that I assumed every mst has just one center. we can check at print(which.min(maxdist)).
   
-  plotcenter = plot(center)
-  
+  return(center)
   
   frequency = table(center)
-  plotcenter = plot(frequency)
+  plot(frequency)
 
 }
 
